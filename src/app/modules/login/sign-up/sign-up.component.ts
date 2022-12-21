d2dc5651359b393d877ecc02c486eb8fa16da9cb
rdbruno <brunodaniele.br@gmail.com>
2022-12-21 19:50:27 -0300

    SignUp Page

--- src/app/modules/login/sign-up/sign-up.component.ts
+++ src/app/modules/login/sign-up/sign-up.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  public formSignUp = this._formBuilder.group({
+    name: [null, [ Validators.required ]],
+    email: [null, [ Validators.required, Validators.email ]],
+    password: [null, [ Validators.required ]]
+  });
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+    private _snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public onSignUp(): void {
+
+  }
+
+  public onSignInPage(): void {
+    this._router.navigate(['login/signin']);
   }
 
 }
